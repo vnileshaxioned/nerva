6c84f9bbd944a7e9a40e40954998a778ad531513
Nilesh Vishwakarma <nileshv@axioned.com>
2022-06-15 19:22:34 +0530

    done with filter tab

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,23 @@
 /* Author: 
 
 */
+var categories = document.querySelectorAll('.category-list');
+var selectedCategory = document.querySelectorAll('.show-collection');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+selectedCategory.forEach(function (select) {
+  categories.forEach(function (category) {
+      if (select.dataset.category == category.dataset.category) {
+        category.addEventListener('click', function () {
+          select.classList.remove('collection-inactive')
+        })
+      } else if (category.dataset.category == 'all') {
+        category.addEventListener('click', function () {
+          select.classList.remove('collection-inactive')
+        })
+      } else {
+        category.addEventListener('click', function () {
+          select.classList.add('collection-inactive')
+        })
+      }
+  })
+})
