203fb6d2bb45c5242dd1bb94648e7073322431f6
Nilesh Vishwakarma <nileshv@axioned.com>
2022-06-18 14:55:12 +0530

    modal append using js

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,21 +6,46 @@ for (var images of selectedImage) {
   images.classList.add('collection-active');
 }
 
-categories.forEach(function (category) {
-  var clicked = category.dataset.category;
-    category.addEventListener('click', function () {
-      for (var images of selectedImage) {
-        if (clicked == 'all') {
-          images.classList.add('collection-active');
-        } else if (images.classList.contains(clicked)) {
-          images.classList.add('collection-active');
-        } else {
-          images.classList.remove('collection-active');
-        }
+for (i = 0; i < categories.length; i++) {
+  categories[i].addEventListener('click', function () {
+    for (j = 0; j < categories.length; j++) {
+      categories[j].classList.remove('category-active');
+    }
+    this.classList.add('category-active');
+    var dataCategory = this.getAttribute('data-category');
+    for (var images of selectedImage) {
+      images.classList.remove('collection-active');
+      if (images.getAttribute('data-name') == dataCategory || dataCategory == 'all') {
+        images.classList.add('collection-active');
       }
-    });
-});
+    }
+  })
+}
 // category tab filter ends here
+
+// for modal
+var gallery = document.querySelector('.gallery > .wrapper');
+var modal = document.createElement("ul");
+modal.className = "modal";
+gallery.appendChild(modal);
+
+for (i = 0; i < selectedImage.length; i++) {
+  selectedImage[i].addEventListener('click', function () {
+    for (j = 0; j < selectedImage.length; j++) {
+      selectedImage[j].classList.remove('modal-active');
+    }
+    this.classList.add('modal-active');
+    console.log(this);
+    var selectedModal = document.querySelector('.modal');
+    var collections = document.querySelector('.modal-active');
+    newCollection = collections.cloneNode(true);
+    selectedModal.appendChild(newCollection);
+  })
+}
+
+
+
+// modal ends here
 
 // for modal
 selectedImage.forEach(function (image) {
