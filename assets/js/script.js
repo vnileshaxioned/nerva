9e5b004d80bef967669b2352a6e6305cca9226c8
Nilesh Vishwakarma <nileshv@axioned.com>
2022-06-19 21:35:13 +0530

    added modal left right button

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,21 +25,42 @@ for (i = 0; i < categories.length; i++) {
 
 // for modal
 var gallery = document.querySelector('.gallery > .wrapper');
+var container = document.querySelector('.container');
 var modal = document.createElement("ul");
 modal.className = "modal";
-gallery.appendChild(modal);
+var modalClose = document.createElement("a");
+modalClose.setAttribute('href', '#FIXME');
+modalClose.className = "close";
+modalClose.setAttribute('title', 'Close');
+modalClose.textContent = "close";
 
 for (i = 0; i < selectedImage.length; i++) {
   selectedImage[i].addEventListener('click', function () {
     for (j = 0; j < selectedImage.length; j++) {
       selectedImage[j].classList.remove('modal-active');
     }
+    gallery.appendChild(modal);
     this.classList.add('modal-active');
-    console.log(this);
+    container.className = "container-active";
     var selectedModal = document.querySelector('.modal');
     var collections = document.querySelector('.modal-active');
     newCollection = collections.cloneNode(true);
     selectedModal.appendChild(newCollection);
+    var activeModal = document.querySelector('.modal > .modal-active');
+    activeModal.appendChild(modalClose);
+    var closeModal = document.querySelector('.close');
+    closeModal.addEventListener('click', function () {
+      activeModal.parentNode.removeChild(activeModal);
+      container.classList.remove('container-active');
+      modal.parentNode.removeChild(modal);
+    })
+    var modalButton = activeModal.innerHTML;
+    modalButton += '<ul class="modal-action">';
+    modalButton += '<li><a href="#FIXME" class="prev" title="previous">previous</a></li>';
+    modalButton += '<li><a href="#FIXME" class="next" title="next">next</a></li>';
+    modalButton += '</ul>';
+
+    activeModal.innerHTML = modalButton;
   })
 }
 
